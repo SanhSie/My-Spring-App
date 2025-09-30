e2013d73a62359c15e738cab6a6677d4c100da15
Your Name <you@example.com>
2025-10-01 04:05:54 +0700

    Initial commit

---
+++ src/test/java/com/sie/starterspringboot/StarterSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sie.starterspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
